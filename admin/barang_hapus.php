c9c2a815b7aa979311bc7ee12f78dc96481081ae
Akbar Wijaya <151990608+akbarwjyy@users.noreply.github.com>
2025-06-22 00:54:22 +0700

    fix bug hapus barang

--- admin/barang_hapus.php
+++ admin/barang_hapus.php
@@ -11,30 +11,51 @@ if (!isset($_SESSION['id_admin'])) {
 if (isset($_GET['id'])) {
     $id = mysqli_real_escape_string($conn, $_GET['id']);
 
+    // --- PERBAIKAN DI SINI ---
     // Ambil informasi gambar sebelum menghapus
-    $query = "SELECT gambar FROM sepatu WHERE id_sepatu = ?";
-    $stmt = mysqli_prepare($conn, $query);
-    mysqli_stmt_bind_param($stmt, "i", $id);
-    mysqli_stmt_execute($stmt);
-    $result = mysqli_stmt_get_result($stmt);
-    $sepatu = mysqli_fetch_assoc($result);
+    // Ubah dari 'sepatu' menjadi 'barang' dan 'id_sepatu' menjadi 'id_barang'
+    $query_select_gambar = "SELECT gambar FROM barang WHERE id_barang = ?";
+    $stmt_select_gambar = mysqli_prepare($conn, $query_select_gambar);
+
+    if (!$stmt_select_gambar) {
+        $_SESSION['error'] = "Gagal menyiapkan query select gambar: " . mysqli_error($conn);
+        header("Location: barang_list.php");
+        exit();
+    }
+
+    mysqli_stmt_bind_param($stmt_select_gambar, "i", $id);
+    mysqli_stmt_execute($stmt_select_gambar);
+    $result_gambar = mysqli_stmt_get_result($stmt_select_gambar);
+    $barang = mysqli_fetch_assoc($result_gambar); // Ubah $sepatu menjadi $barang
 
     // Hapus data dari database
-    $query = "DELETE FROM sepatu WHERE id_sepatu = ?";
-    $stmt = mysqli_prepare($conn, $query);
-    mysqli_stmt_bind_param($stmt, "i", $id);
+    // Ubah dari 'sepatu' menjadi 'barang' dan 'id_sepatu' menjadi 'id_barang'
+    $query_delete = "DELETE FROM barang WHERE id_barang = ?";
+    $stmt_delete = mysqli_prepare($conn, $query_delete);
 
-    if (mysqli_stmt_execute($stmt)) {
-        // Hapus file gambar jika ada
-        if ($sepatu['gambar'] && file_exists("../assets/img/" . $sepatu['gambar'])) {
-            unlink("../assets/img/" . $sepatu['gambar']);
-        }
-        $_SESSION['message'] = "Sepatu berhasil dihapus!";
-    } else {
-        $_SESSION['error'] = "Gagal menghapus sepatu!";
+    if (!$stmt_delete) {
+        $_SESSION['error'] = "Gagal menyiapkan query delete: " . mysqli_error($conn);
+        header("Location: barang_list.php");
+        exit();
     }
+
+    mysqli_stmt_bind_param($stmt_delete, "i", $id);
+
+    if (mysqli_stmt_execute($stmt_delete)) {
+        // Hapus file gambar jika ada
+        // Gunakan $barang['gambar'] dan pastikan file_exists
+        if ($barang && $barang['gambar'] && file_exists("../assets/img/" . $barang['gambar'])) {
+            unlink("../assets/img/" . $barang['gambar']);
+        }
+        $_SESSION['message'] = "Barang berhasil dihapus!"; // Ubah pesan sukses
+    } else {
+        $_SESSION['error'] = "Gagal menghapus barang: " . mysqli_error($conn); // Ubah pesan error
+    }
+
+    mysqli_stmt_close($stmt_select_gambar);
+    mysqli_stmt_close($stmt_delete);
 } else {
-    $_SESSION['error'] = "ID sepatu tidak valid!";
+    $_SESSION['error'] = "ID barang tidak valid!"; // Ubah pesan error
 }
 
 header("Location: barang_list.php");
--- user/logout.php
+++ user/logout.php
@@ -1,4 +1,5 @@
 <?php
-session_start();
-session_destroy();
-header("Location: login_user.php");
+session_start(); // Sudah ada
+session_destroy(); // Sudah ada
+header("Location: login_user.php"); // Sudah benar, mengarah ke user/login_user.php
+exit();
